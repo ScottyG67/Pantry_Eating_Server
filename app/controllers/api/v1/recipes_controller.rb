f700847420a3d897d130aee362489c11cd4948db
github username <scott.gloyna01@gmail.com>
2021-04-08 15:31:03 -0500

    add rest-client gem

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -1,7 +1,11 @@
+require 'uri'
+require 'net/http'
+require 'openssl'
+
 class Api::V1::RecipesController < ApplicationController
 
     def index
-        byebug
-        render json: {message: "here"}
+        resp = RestClient.get 'https://api.edamam.com/search?q=chicken&app_id=9db917fd&app_key=c621a1acced53c09facd07f43ef85b0d'
+        render json: resp
     end
 end
